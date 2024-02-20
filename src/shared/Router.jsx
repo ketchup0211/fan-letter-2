5ea4bd9e7792bd0cc205e7897e7f75ac26d97c17
ketchup0211 <dlcka1111@naver.com>
2024-02-21 04:20:39 +0900

    [ADD] pages > login > Login.jsx and [MOD] shared > Router.jsx Route and [MOD] routing 관련 task들

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "@/pages/home/Home.jsx";
 import Detail from "../pages/detail/Detail.jsx";
+import Login from "@/pages/login/Login.jsx";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/detail/:receiver/:id" element={<Detail />} />
       </Routes>
     </BrowserRouter>
