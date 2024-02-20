0d58a6293cec728cba782c886d261c34b26a6165
ketchup0211 <dlcka1111@naver.com>
2024-02-21 04:41:32 +0900

    [ADD] Login.jsx -> Login UI Complete

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,4 +1,19 @@
+import style from "./Login.module.css";
 function Login() {
-  return <></>;
+  return (
+    <div className={style.body}>
+      <div className={style.container}>
+        <p className={style.title}>로그인</p>
+        <form className={style.form}>
+          <input className={style.input} placeholder="ID" />
+          <input className={style.input} placeholder="PW" />
+        </form>
+        <div className={style.buttons}>
+          <button className={style.button}>로그인</button>
+          <button className={style.button}>회원가입</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Login;
