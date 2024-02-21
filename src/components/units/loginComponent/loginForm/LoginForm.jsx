01ee25bacb9670b309e02db6dbe0cf066a55f0b8
ketchup0211 <dlcka1111@naver.com>
2024-02-21 12:41:30 +0900

    [ADD] validation check and button disabled setting

--- src/components/units/loginComponent/loginForm/LoginForm.jsx
+++ src/components/units/loginComponent/loginForm/LoginForm.jsx
@@ -1,6 +1,26 @@
+import { useState } from "react";
 import style from "./LoginForm.module.css";
 function LoginForm({ setLogin }) {
-  const handleLogin = () => {};
+  const [userID, setUserID] = useState("");
+  const [userPW, setUserPW] = useState("");
+  const filled = userID !== "" && userPW !== "";
+  const handleInput = (event) => {
+    event.preventDefault();
+    switch (event.target.name) {
+      case "id":
+        setUserID(event.target.value);
+        break;
+      case "password":
+        setUserPW(event.target.value);
+        break;
+      default:
+        return;
+    }
+  };
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+  };
 
   const handleToggleSignUp = () => {
     setLogin(false);
@@ -9,13 +29,31 @@ function LoginForm({ setLogin }) {
     <>
       <p className={style.title}>로그인</p>
       <form className={style.form}>
-        <input className={style.input} placeholder="ID" />
-        <input className={style.input} placeholder="PW" />
+        <input
+          className={style.input}
+          placeholder="ID"
+          value={userID}
+          onChange={handleInput}
+          type="text"
+          name="id"
+          autoComplete="off"
+        />
+        <input
+          className={style.input}
+          placeholder="PW"
+          value={userPW}
+          onChange={handleInput}
+          type="password"
+          name="password"
+        />
       </form>
       <div className={style.buttons}>
         <button
-          className={`${style.button} ${style.active}`}
+          className={`${style.button} ${
+            !filled ? style.disabled : style.active
+          }`}
           onClick={handleLogin}
+          disabled={!filled}
         >
           로그인
         </button>
