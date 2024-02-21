00b8eff0008db338d9115632a385aaf50f25cf5f
ketchup0211 <dlcka1111@naver.com>
2024-02-22 01:33:44 +0900

    [ADD] json-server를 통한 letter create(post)구현

--- src/components/units/homeComponent/homeBody/telegramList/TelegramList.jsx
+++ src/components/units/homeComponent/homeBody/telegramList/TelegramList.jsx
@@ -2,10 +2,24 @@ import { useSelector } from "react-redux";
 import TelegramBoxes from "./telegramBoxes/TelegramBoxes";
 import getLocalData from "@/util/getLocalData";
 import style from "./TelegramList.module.css";
+import axios from "axios";
+const DB_URL = "http://localhost:4000";
 
 function TelegramList() {
   const receiver = useSelector((state) => state.MainDataReducer.receiver);
   const data = getLocalData(receiver);
+  const getLetters = async () => {
+    try {
+      const response = await axios.get(DB_URL + "/letters");
+      console.log("getLetters");
+      console.log(response.data.filter((item) => item.receiver === receiver));
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  };
+  getLetters();
   return (
     <div className={style.container}>
       {data.length > 0 ? (
